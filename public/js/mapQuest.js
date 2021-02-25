aa9629610433dc5ecd7b6f766966051feb25360f
Simonne Contreras <sccontre@ucsd.edu>
2021-02-25 10:35:16 -0800

    thirdpartyAPI Lab

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,13 @@
 function initMap() {
-	// add your code here
+	L.mapquest.key = 'PEcQf2gacA9P0hRAxjYj1B5ZsvjIpfmc';
+
+	// 'map' refers to a <div> element with the ID map
+	var map = L.mapquest.map('map', {
+		center: [32.87870949762811, -117.23581090068045],
+		layers: L.mapquest.tileLayer('map'),
+		zoom: 14,
+		zoomControl: false
+	});
+
+	L.marker([32.88, -117.236]).addTo(map);
 }
